685fcd6dbb787d0f78f6f0d5a0c67d1cf89c5de3
reyazesheik <reyazesheik3106@gmail.com>
2026-07-11 11:06:17 +0530

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,9 @@
+variable "aws_region" {
+  description = "AWS Region"
+  default     = "ap-south-1"
+}
+
+variable "instance_type" {
+  description = "EC2 Instance Type"
+  default     = "t2.micro"
+}
