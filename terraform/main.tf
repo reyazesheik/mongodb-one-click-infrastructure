63f160a6a03a5184106d0d196ee3225ecf0d2cc8
reyazesheik <reyazesheik3106@gmail.com>
2026-07-11 17:34:34 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,12 @@
 resource "aws_instance" "mongodb" {
-  ami           = "ami-0f58b397bc5c1f2e8"
+  ami           = "ami-05f8b397bc5c1f2e8"
   instance_type = "t2.micro"
+
+  key_name = "Mongodb2-key"
+
+  subnet_id = "subnet-00bf57a73c2c10c17"
+
+  associate_public_ip_address = true
 
   tags = {
     Name = "MongoDB-Server"
